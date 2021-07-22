142c28f1eac061a32f3df56d4cc28bd0a2f786e9
Poker <1149739343@qq.com>
2021-07-22 19:26:05 +0800

    导航栏

---
+++ bpproject/Assets/Scripts/TableBar.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 导航栏组件类。
+/// </summary>
+public class TableBar : MonoBehaviour
+{
+    #region 可视变量
+    public int size = 0;
+    public int defaultIndex = 0;
+    public Button[] buttons = new Button[100];
+    public GameObject[] panels = new GameObject[100];
+    #endregion
+
+    #region 成员变量
+    private int selected = 0;   // 当前选择的面板序号
+    #endregion
+
+    #region 私有方法
+    /// <summary>
+    /// 脚本实例化后立即触发。
+    /// </summary>
+    private void Awake()
+    {
+        selected = defaultIndex;
+        // 绑定按钮监听事件
+        for (int i = 0; i < size; i++)
+        {
+            int temp = i;
+            panels[temp].SetActive(false);
+            buttons[temp].onClick.AddListener(delegate () { OnClick(temp); });
+        }
+    }
+
+    /// <summary>
+    /// 当脚本生效时触发。
+    /// </summary>
+    private void OnEnable()
+    {
+        // 仅激活默认面板
+        SelectPanel(defaultIndex);
+    }
+
+    /// <summary>
+    /// 按钮被单击时触发。
+    /// </summary>
+    /// <param name="index">按钮序号。</param>
+    private void OnClick(int index)
+    {
+        SelectPanel(index);
+    }
+
+    /// <summary>
+    /// 切换面板。
+    /// </summary>
+    /// <param name="index">面板序号。</param>
+    private void SelectPanel(int index)
+    {
+        panels[selected].SetActive(false);
+        panels[index].SetActive(true);
+        selected = index;
+    }
+    #endregion
+}
