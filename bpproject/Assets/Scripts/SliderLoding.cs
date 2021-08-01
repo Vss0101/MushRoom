cb860ab560d53f3c14569538c1e91326f4d21dbe
sola <sola@forevernine.com>
2021-08-01 22:10:46 +0800

    进度条实现

---
+++ bpproject/Assets/Scripts/SliderLoding.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class SliderLoding : MonoBehaviour
+{
+    private int cur;
+    public Text text;
+    public Slider slider;
+    public GameObject LoadingP;
+    public GameObject GameMain;
+
+
+	void Start () 
+    {
+        cur = 0;
+	}
+	void Update () 
+    {
+        //单位时间内刷新
+        Invoke("Load",5);
+	}
+
+    //迭代刷新
+    void Load(){
+        if(cur>=100){
+            LoadingP.SetActive(false);
+            GameMain.SetActive(true);
+        }
+        cur = cur + 1;
+        slider.value = cur;
+        //显示百分比
+        text.text = cur.ToString() + "%";
+    }
+}
