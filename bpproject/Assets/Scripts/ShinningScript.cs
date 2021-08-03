872c9d898909e58105ed1d1e89589df4358f904c
constantine <constantine_27149@163.com>
2021-08-03 09:39:25 +0800

    add start button shinning

---
+++ bpproject/Assets/Scripts/ShinningScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShinningScript : MonoBehaviour
+{
+    private CanvasGroup moonCanvasGroup;
+    private float flashSpeed = 0.2f;//光晕闪动速度
+    private bool isOn = true;
+    private float maxAlpha = 0.9f;//显示的最高alpha值
+    private float minAlpha = 0.05f;//显示的最低alpha值
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moonCanvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moonCanvasGroup.alpha < maxAlpha && isOn)
+        {
+            moonCanvasGroup.alpha += flashSpeed * Time.deltaTime;
+        }
+        else
+        {
+            isOn = false;
+            moonCanvasGroup.alpha -= flashSpeed * Time.deltaTime;
+            if (moonCanvasGroup.alpha < minAlpha)
+            {
+                isOn = true;
+            }
+        }
+    }
+
+    }
+
