fbe4e7f342f2778ec853c1c9c788a29e178b8814
Poker <1149739343@qq.com>
2021-08-04 09:04:59 +0800

    淡入淡出

---
+++ bpproject/Assets/Scripts/BuildingTop/ButtonDown.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonDown : MonoBehaviour
+{
+    public Button down;
+    public GameObject switchBackround;
+    public GameObject middlePanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        down.onClick.AddListener(delegate () { OnClick(); });
+    }
+
+    public void OnClick()
+    {
+        switchBackround.SetActive(true);
+        switchBackround.GetComponent<Animator>().Play("FadeOut");
+        Invoke("ReverseActive",2f);
+        Invoke("SwitchPanel", 1f);
+    }
+
+    public void SwitchPanel()
+    {
+        middlePanel.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void ReverseActive()
+    {
+        switchBackround.SetActive(false);
+        //gameObject.SetActive(false);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
