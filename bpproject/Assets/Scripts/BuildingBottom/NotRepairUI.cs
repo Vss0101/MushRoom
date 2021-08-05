80b70e801febcc5046ff5fad9103c51c8bc169b0
Poker <Poker@aladin.com>
2021-08-06 00:29:31 +0800

    主页面建筑修复UI

---
+++ bpproject/Assets/Scripts/BuildingBottom/NotRepairUI.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class NotRepairUI : MonoBehaviour
+{
+    public Button bFire;
+    public Button bWater;
+    public Button bWind;
+    public Button bLand;
+    public Image resource;
+    public Sprite sFire;
+    public Sprite sWater;
+    public Sprite sWind;
+    public Sprite sLand;
+    public Button ok;
+    public Button cancel;
+    // Start is called before the first frame update
+    void Start()
+    {
+       bFire.onClick.AddListener(delegate () { OnClickFire(); });
+       bWater.onClick.AddListener(delegate () { OnClickWater(); });
+       bWind.onClick.AddListener(delegate () { OnClickWind(); });
+       bLand.onClick.AddListener(delegate () { OnClickLand(); });
+       ok.onClick.AddListener(delegate () { OnClickOK(); });
+       cancel.onClick.AddListener(delegate () { OnClickCancel(); });
+    }
+
+    public void OnClickCancel(){
+        gameObject.transform.DOMove(new Vector3(gameObject.transform.position.x+800,gameObject.transform.position.y),0.5f);
+    }
+
+    public void OnClickOK(){
+        gameObject.transform.DOMove(new Vector3(gameObject.transform.position.x+800,gameObject.transform.position.y),0.5f);
+    }
+
+    public void OnClickFire(){
+        resource.sprite = sFire;
+    }
+
+    public void OnClickWater(){
+        resource.sprite = sWater;
+    }
+
+    public void OnClickLand(){
+        resource.sprite = sLand;
+    }
+
+    public void OnClickWind(){
+        resource.sprite = sWind;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ bpproject/Assets/Scripts/BuildingBottom/PopUI.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class PopUI : MonoBehaviour
+{
+    public Button building;
+    public Button wantRepair;
+    public GameObject notRepair;
+    public GameObject repair;
+    public Image buildingA;
+    public bool isOpen;
+    public float scaleForUI;
+    public float scaleForBuilding;
+    private bool isRepair;
+    public GameObject notRepairUI;
+    public GameObject center;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isRepair = false;
+        scaleForBuilding = 1;
+        building.onClick.AddListener(delegate () { OnClickForBuilding(); });
+        wantRepair.onClick.AddListener(delegate () { OnClickForWantRepair(); });
+    }
+
+    public void OnClickForWantRepair()
+    {
+        isRepair = true;
+        if(scaleForUI<0){
+            scaleForUI = -scaleForUI;
+        }
+        buildingScaleControl();
+        notRepair.transform.DOScale(new Vector3(0, 0, 0), 0.5f);
+    }
+
+    public void buildingScaleControl()
+    {
+        building.transform.DOScale(new Vector3(building.transform.localScale.x + scaleForBuilding, building.transform.localScale.y + scaleForBuilding), 0.5f);
+        scaleForBuilding = -scaleForBuilding;
+    }
+
+    public void OnClickForBuilding()
+    {
+        //buildingScaleControl();
+        if (!isRepair)
+        {
+            notRepairUI.transform.DOMove(new Vector3(center.transform.position.x,notRepairUI.transform.position.y),0.5f);
+            //notRepair.transform.DOScale(new Vector3(notRepair.transform.localScale.x+scaleForUI, notRepair.transform.localScale.x+scaleForUI), 0.5f);
+            //scaleForUI = -scaleForUI;
+        }
+        else
+        {
+            buildingScaleControl();
+            repair.transform.DOScale(new Vector3(repair.transform.localScale.x+scaleForUI, repair.transform.localScale.x+scaleForUI), 0.5f);
+            scaleForUI = -scaleForUI;
+        }
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
