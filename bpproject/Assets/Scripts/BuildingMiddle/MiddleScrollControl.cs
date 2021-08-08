fe4f4e1c4ae0f2a73aa628f5d008487ea0f31f39
Poker <Poker@aladin.com>
2021-08-08 16:55:11 +0800

    建筑页中部实现点击按钮让关卡在屏幕中央

---
+++ bpproject/Assets/Scripts/BuildingMiddle/MiddleScrollControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MiddleScrollControl : MonoBehaviour
+{
+    public Button move;
+    public GameObject center;
+    // Start is called before the first frame update
+    void Start()
+    {
+        move.onClick.AddListener(delegate () { OnClickMove(); });
+    }
+
+    public void OnClickMove(){
+        gameObject.transform.DOMove(new Vector3(gameObject.transform.position.x,gameObject.transform.position.y-(move.transform.position.y-center.transform.position.y)),0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
