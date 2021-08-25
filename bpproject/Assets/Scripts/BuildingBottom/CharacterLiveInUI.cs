cfdb69bb052a6a816334f9bd4b2ef5c9aad61b20
Poker <Poker@aladinfun.com>
2021-08-25 17:33:31 +0800

    场景优化及同步

--- bpproject/Assets/Scripts/BuildingBottom/CharacterLiveInUI.cs
+++ bpproject/Assets/Scripts/BuildingBottom/CharacterLiveInUI.cs
@@ -12,12 +12,20 @@ public class CharacterLiveInUI : MonoBehaviour
     public Image character1;
     public Image character2;
     public Image character3;
+    public Image character4;
+
     public Button right;
     public Button left;
     public GameObject content;
     private int selectCharacter;
     public CanvasGroup rightCanvas;
     public CanvasGroup leftCanvas;
+
+    public Sprite s1;
+    public Sprite s2;
+    public Sprite s3;
+    public Sprite s4;
+    public Image image;
 
 
     // Start is called before the first frame update
@@ -29,6 +37,7 @@ public class CharacterLiveInUI : MonoBehaviour
         character1.GetComponent<Button>().onClick.AddListener(delegate () { OnClickSelectC1(); });
         character2.GetComponent<Button>().onClick.AddListener(delegate () { OnClickSelectC2(); });
         character3.GetComponent<Button>().onClick.AddListener(delegate () { OnClickSelectC3(); });
+        character4.GetComponent<Button>().onClick.AddListener(delegate () { OnClickSelectC4(); });
         right.GetComponent<Button>().onClick.AddListener(delegate () { OnClickGoRight(); });
         left.GetComponent<Button>().onClick.AddListener(delegate () { OnClickGoLeft(); });
         rightCanvas = right.GetComponent<CanvasGroup>();
@@ -39,27 +48,56 @@ public class CharacterLiveInUI : MonoBehaviour
 
     public void OnClickSelectC1()
     {
-
+        image.sprite = s1;
+        gameObject.transform.DOMove(new Vector3(gameObject.transform.position.x + 800, gameObject.transform.position.y), 0.5f);
     }
     public void OnClickSelectC2()
     {
-
+        image.sprite = s2;
+        gameObject.transform.DOMove(new Vector3(gameObject.transform.position.x + 800, gameObject.transform.position.y), 0.5f);
     }
     public void OnClickSelectC3()
     {
-
+        image.sprite = s3;
+        gameObject.transform.DOMove(new Vector3(gameObject.transform.position.x + 800, gameObject.transform.position.y), 0.5f);
+    }
+    public void OnClickSelectC4()
+    {
+        image.sprite = s4;
+        gameObject.transform.DOMove(new Vector3(gameObject.transform.position.x + 800, gameObject.transform.position.y), 0.5f);
     }
 
     public void OnClickGoRight()
     {
-        content.transform.DOMove(new Vector3(content.transform.position.x - 465, content.transform.position.y), 0.5f);
+        Tween t =content.transform.DOMove(new Vector3(content.transform.position.x - 465, content.transform.position.y), 0.5f);
+        right.enabled = false;
+        left.enabled = false;
+        t.OnComplete(
+           () =>
+           {
+               right.enabled = true;
+               left.enabled = true;
+               CheckEnable();
+           }
+       );
         selectCharacter++;
     
     }
 
     public void OnClickGoLeft()
     {
-        content.transform.DOMove(new Vector3(content.transform.position.x + 465, content.transform.position.y), 0.5f);
+        Tween t = content.transform.DOMove(new Vector3(content.transform.position.x + 465, content.transform.position.y), 0.5f);
+        right.enabled = false;
+        left.enabled = false;
+        t.OnComplete(
+           () =>
+           {
+               right.enabled = true;
+               left.enabled = true;
+               CheckEnable();
+           }
+       );
+
         selectCharacter--;
     }
 
@@ -80,11 +118,18 @@ public class CharacterLiveInUI : MonoBehaviour
     void Update()
     {
        
-        if(selectCharacter == 1)
+        
+
+    }
+
+    public void CheckEnable()
+    {
+        if (selectCharacter == 1)
         {
             leftCanvas.alpha = 0.5f;
             left.enabled = false;
-        }else if (selectCharacter == 4)
+        }
+        else if (selectCharacter == 4)
         {
             rightCanvas.alpha = 0.5f;
             right.enabled = false;
@@ -96,7 +141,6 @@ public class CharacterLiveInUI : MonoBehaviour
             rightCanvas.alpha = 1f;
             right.enabled = true;
         }
-
     }
 
     public void OnClick(BaseEventData data)
