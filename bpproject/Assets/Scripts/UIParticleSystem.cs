be27d0d3446b98cea411be7739d5d667e8a8744e
Poker <Poker@aladinfun.com>
2021-08-31 20:37:18 +0800

    增加开始页特效及角色页特效及建筑升级特效

--- bpproject/Assets/Scripts/UIParticleSystem.cs
+++ bpproject/Assets/Scripts/UIParticleSystem.cs
@@ -71,7 +71,7 @@ namespace UnityEngine.UI.Extensions
                     pRenderer.enabled = false;
 
                 Shader foundShader = Shader.Find("UI Extensions/Particles/Additive");
-                Material pMaterial = new Material(foundShader);
+                Material pMaterial = new Material(foundShader);git
 
                 if (material == null)
                     material = pMaterial;
