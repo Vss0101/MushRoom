f60d497b99ed389f7012ba188814e517579d87b3
Poker <Poker@aladinfun.com>
2021-09-12 16:36:12 +0800

    老虎机页面UI整体优化+开始界面背景替换

---
+++ bpproject/Assets/Scripts/Slot/HighLightControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class HighLightControl : MonoBehaviour
+{
+    public bool run;
+    private CanvasGroup moonCanvasGroup;
+    void Start()
+    {
+        moonCanvasGroup = GetComponent<CanvasGroup>();
+        run = true;
+        moonCanvasGroup.alpha = 0;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       if (run)
+        {
+            run = false;
+            Tween t = DOTween.To(() => moonCanvasGroup.alpha, x => moonCanvasGroup.alpha = x, 0.6f, 0.5f);
+            t.OnComplete(
+                () =>
+                {
+                   Tween z = DOTween.To(() => moonCanvasGroup.alpha, x => moonCanvasGroup.alpha = x, 0f, 0.5f);
+                    z.OnComplete(
+               () =>
+               {
+                   gameObject.SetActive(false);
+                   run = true;
+               }
+           );
+                }
+            );
+        }
+
+    }
+}
